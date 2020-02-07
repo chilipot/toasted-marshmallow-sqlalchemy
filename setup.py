41d59497446bbbfce8af624c931baf13d0e47961
Daniel Guddemi <danguddemi@gmail.com>
2020-02-06 22:50:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import re
 from setuptools import setup, find_packages
 
 
-INSTALL_REQUIRES = ("marshmallow>=2.15.2", "SQLAlchemy>=1.2.0")
+INSTALL_REQUIRES = ("toastedmarshmallow>=2.15.2", "SQLAlchemy>=1.2.0")
 EXTRAS_REQUIRE = {
     "tests": ["pytest", "mock"],
     "lint": ["flake8==3.7.9", "flake8-bugbear==20.1.4", "pre-commit~=2.0"],
